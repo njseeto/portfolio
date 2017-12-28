d879a84ec67ac733c5e24ef3be8ac1d39e8d9a76
Natalie <njseeto@users.noreply.github.com>
2017-12-28 21:03:11 +0000

    smooth scrolling function added

--- logic.js
+++ logic.js
@@ -57,3 +57,36 @@ window.onload = function() {
 };
 
 
+//JQuery
+// smooth scrolling
+
+$('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          var $target = $(target);
+        
+          if ($target.is(":focus")) { 
+            return false;
+          } else {
+            $target.attr('tabindex','-1');
+          };
+        });
+      }
+    }
+  });
